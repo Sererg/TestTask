0eed0645a2d343c11abe95bd0fe4f0ac46931493
Sergey <svovyk@mail.ru>
2024-02-19 13:05:25 +0300

    Контактная форма

---
+++ sitePhp/about.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Контактная форма</title>
+</head>
+<body>
+    <?php require "blocks/header.php"?>
+    <div class="container mt-5">
+        <h3>Контактная форма</h3>
+        <form action="check.php" method="post">
+            <input type="email" name="email" placeholder="Введите Email"
+            class="form-control"><br>
+            <textarea name="message" class="form-control" placeholder="Введите ваше соообщение"></textarea><br>
+            <button type="submit" name="send" class="btn btn-success">Отправить</button>
+        </form>
+    </div>
+   
+
+
+    <?php require "blocks/footer.php"?>
+   
+</body>
+</html>
