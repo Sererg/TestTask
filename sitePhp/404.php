3066a55430135642101a8ca9993ee20e48b9d9bc
Sergey <svovyk@mail.ru>
2024-02-19 13:08:46 +0300

    страница ошибки

---
+++ sitePhp/404.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>PHP веб сайт</title>
+</head>
+<body>
+    <?php require "blocks/header.php"?>
+    <div class="container">
+        <h3 class="mb-5">Ошибка 404</h3>
+    </div>
+    <?php require "blocks/footer.php"?>
+   
+</body>
+</html>
